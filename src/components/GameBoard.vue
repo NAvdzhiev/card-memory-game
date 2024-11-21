ebc9e8181d695488cb27a43db2ee2f970699e20f
NAvdzhiev <artcoder101@gmail.com>
2024-11-21 12:07:10 +0200

    Shuffle Cards logic added

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -12,23 +12,13 @@
 
 <script setup>
 import SingleCard from './SingleCard.vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useGameStore } from '@/store/gameStore';
 
 const gameStore = useGameStore();
+const shuffledCards = computed(() => gameStore.getShuffledCards);
 const flippedCards = ref([]);
 const matchedCards = ref([]);
-
-const cards = [
-	{ id: 1, image: 'image1.jpg', text: 'Image 1' },
-	{ id: 2, image: 'image2.jpg', text: 'Image 2' },
-	{ id: 3, image: 'image3.jpg', text: 'Image 3' },
-	{ id: 4, image: 'image4.jpg', text: 'Image 4' },
-	{ id: 1, image: 'image1.jpg', text: 'Image 5' },
-	{ id: 2, image: 'image2.jpg', text: 'Image 6' },
-	{ id: 3, image: 'image3.jpg', text: 'Image 7' },
-	{ id: 4, image: 'image4.jpg', text: 'Image 8' },
-];
 </script>
 
 <style scoped>
--- src/store/gameStore.js
+++ src/store/gameStore.js
@@ -4,7 +4,17 @@ export const useGameStore = defineStore('game', {
 	state: () => ({
 		difficulty: 'easy',
 		theme: 'classic',
-		cards: [],
+		cards: [
+			{ id: 1, image: 'image1.jpg', text: 'Image 1' },
+			{ id: 2, image: 'image2.jpg', text: 'Image 2' },
+			{ id: 3, image: 'image3.jpg', text: 'Image 3' },
+			{ id: 4, image: 'image4.jpg', text: 'Image 4' },
+			{ id: 1, image: 'image1.jpg', text: 'Image 1' },
+			{ id: 2, image: 'image2.jpg', text: 'Image 2' },
+			{ id: 3, image: 'image3.jpg', text: 'Image 3' },
+			{ id: 4, image: 'image4.jpg', text: 'Image 4' },
+		],
+		shuffledCards: [],
 		time: 0,
 		timeInterval: null,
 		attempts: 0,
@@ -14,13 +24,25 @@ export const useGameStore = defineStore('game', {
 	actions: {
 		startGame() {
 			this.time = 0;
-			this.gameStatus = 'in progress';
+			this.generateCards();
 			this.timeInterval = setInterval(() => {
 				this.time++;
 			}, 1000);
 		},
+
+		generateCards() {
+			//Shuffle cards to randomize their order
+			this.cards.sort(() => Math.random() - 0.5);
+			// Fill shuffeld cards into shuffledCards array
+			this.shuffledCards = this.cards.map((icon) => ({
+				id: icon.id,
+				icon: icon.image,
+				text: icon.text,
+			}));
+		},
 	},
 	getters: {
 		getTime: (state) => state.time,
+		getShuffledCards: (state) => state.shuffledCards,
 	},
 });
