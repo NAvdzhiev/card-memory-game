92f7e565cb7de43059c9866354a8244827f83c0c
NAvdzhiev <artcoder101@gmail.com>
2024-11-22 18:17:30 +0200

    Flip sound changed, theme backgrounds added

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,55 @@
 <template>
-	<header class="header" v-if="gameStore.gameStatus !== 'start'">
-		<nav class="nav">
-			<ul class="nav__menu">
-				<li class="nav__menu-item">
-					<AppButton title="Restart Game" @click="newGame" />
-				</li>
-				<li class="nav__menu-item">
-					<TimeCounter />
-					<AttemptCounter />
-				</li>
-				<li class="nav__menu-item">
-					<button @click="openDialog('records')">Records</button>
-				</li>
-			</ul>
-		</nav>
-	</header>
-	<main class="container">
-		<section v-if="gameStore.gameStatus === 'start'" class="container__start">
-			<AppButton
-				v-if="gameStore.gameStatus === 'start'"
-				title="Start Game"
-				@click="gameStore.startGame"
-			/>
-			<DropdownFilters v-if="gameStore.gameStatus === 'start'" />
-		</section>
-		<section v-else class="container__board">
-			<div v-if="gameStore.gameStatus === 'in progress'">
-				<button @click="gameStore.pauseGame">&#x23F8;</button>
-			</div>
-			<div v-else-if="gameStore.gameStatus === 'paused'">
-				<button @click="gameStore.resetGame">&#x23F5;</button>
-			</div>
-			<GameBoard v-if="gameStore.gameStatus !== 'start'" />
-		</section>
-	</main>
+	<div class="game-app" :style="backgroundStyle">
+		<header class="header" v-if="gameStore.gameStatus !== 'start'">
+			<nav class="nav">
+				<ul class="nav__menu">
+					<li class="nav__menu-item">
+						<AppButton title="Restart Game" @click="newGame" />
+					</li>
+					<li class="nav__menu-item">
+						<TimeCounter />
+						<AttemptCounter />
+					</li>
+					<li class="nav__menu-item">
+						<button @click="openDialog('records')">Records</button>
+					</li>
+				</ul>
+			</nav>
+		</header>
+		<main class="container">
+			<section v-if="gameStore.gameStatus === 'start'" class="container__start">
+				<AppButton
+					v-if="gameStore.gameStatus === 'start'"
+					title="Start Game"
+					@click="gameStore.startGame"
+				/>
+				<DropdownFilters v-if="gameStore.gameStatus === 'start'" />
+			</section>
+			<section v-else class="container__board">
+				<div v-if="gameStore.gameStatus === 'in progress'">
+					<button @click="gameStore.pauseGame">&#x23F8;</button>
+				</div>
+				<div v-else-if="gameStore.gameStatus === 'paused'">
+					<button @click="gameStore.resetGame">&#x23F5;</button>
+				</div>
+				<GameBoard v-if="gameStore.gameStatus !== 'start'" />
+			</section>
+		</main>
 
-	<DialogWindow :open="isDialogOpen" @close="closeDialog">
-		<template v-if="dialogType === 'records'">
-			<BestResults />
-		</template>
-		<template v-else-if="dialogType === 'win'">
-			<h2>Good Job!</h2>
-			<p>
-				You completed the game in {{ gameStore.time }} seconds with
-				{{ gameStore.attempts }} attempts.
-			</p>
-			<AppButton title="Play Again" @click="newGame" />
-		</template>
-	</DialogWindow>
+		<DialogWindow :open="isDialogOpen" @close="closeDialog">
+			<template v-if="dialogType === 'records'">
+				<BestResults />
+			</template>
+			<template v-else-if="dialogType === 'win'">
+				<h2>Good Job!</h2>
+				<p>
+					You completed the game in {{ gameStore.time }} seconds with
+					{{ gameStore.attempts }} attempts.
+				</p>
+				<AppButton title="Play Again" @click="newGame" />
+			</template>
+		</DialogWindow>
+	</div>
 </template>
 
 <script setup>
@@ -60,7 +62,7 @@ import DialogWindow from './components/DialogWindow.vue';
 import DropdownFilters from './components/DropdownFilters.vue';
 
 import { useGameStore } from '@/store/gameStore';
-import { ref, watch } from 'vue';
+import { ref, watch, computed } from 'vue';
 
 const gameStore = useGameStore();
 
@@ -91,9 +93,22 @@ watch(
 		}
 	},
 );
+
+const backgroundStyle = computed(() => ({
+	backgroundImage: `url(${require(`./assets/backgrounds/${gameStore.theme}.jpg`)})`,
+	transition: 'background-image 0.5s ease-in-out', // Transition for smooth change
+}));
 </script>
 
 <style scoped>
+.game-app {
+	background-image: url('./assets/backgrounds/farm.jpg');
+	background-size: cover;
+	background-position: center center;
+	background-repeat: no-repeat;
+	height: 100vh;
+}
+
 nav {
 	width: 100%;
 }
